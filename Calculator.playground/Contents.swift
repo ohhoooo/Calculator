346969cdf000931b79891c4ec0a9143346b9a03c
KJH <gh7052@gmail.com>
2024-03-07 15:58:28 +0900

    Swift 문법 기초 과제 - 계산기 Lv4

--- Calculator.playground/Contents.swift
+++ Calculator.playground/Contents.swift
@@ -1,67 +1,48 @@
 class Calculator {
-    let addOperation: AddOperation
-    let subtractOperation: SubtractOperation
-    let multiplyOperation: MultiplyOperation
-    let divideOperation: DivideOperation
-    let remainderOperation: RemainderOperation
+    var operation: AbstractOperation
     
-    init(
-        addOperation: AddOperation,
-        subtractOperation: SubtractOperation,
-        multiplyOperation: MultiplyOperation,
-        divideOperation: DivideOperation,
-        remainderOperation: RemainderOperation
-    ) {
-        self.addOperation = addOperation
-        self.subtractOperation = subtractOperation
-        self.multiplyOperation = multiplyOperation
-        self.divideOperation = divideOperation
-        self.remainderOperation = remainderOperation
+    init(operation: AbstractOperation) {
+        self.operation = addOperation
     }
     
-    func calculate(oper: String, firstNum: Int, secondNum: Int) -> Double {
-        switch oper {
-        case "+":
-            return addOperation.operate(firstNum: firstNum, secondNum: secondNum)
-        case "-":
-            return subtractOperation.operate(firstNum: firstNum, secondNum: secondNum)
-        case "*":
-            return multiplyOperation.operate(firstNum: firstNum, secondNum: secondNum)
-        case "/":
-            return divideOperation.operate(firstNum: firstNum, secondNum: secondNum)
-        case "%":
-            return remainderOperation.operate(firstNum: firstNum, secondNum: secondNum)
-        default:
-            return 0
-        }
+    func changeOperation(operation: AbstractOperation) {
+        self.operation = operation
+    }
+    
+    func calculate(firstNum: Int, secondNum: Int) -> Double {
+        return operation.operate(firstNum: firstNum, secondNum: secondNum)
     }
 }
 
-class AddOperation {
+protocol AbstractOperation {
+    func operate(firstNum: Int, secondNum: Int) -> Double
+}
+
+class AddOperation : AbstractOperation {
     func operate(firstNum: Int, secondNum: Int) -> Double {
         return Double(firstNum + secondNum)
     }
 }
 
-class SubtractOperation {
+class SubtractOperation : AbstractOperation {
     func operate(firstNum: Int, secondNum: Int) -> Double {
         return Double(firstNum - secondNum)
     }
 }
 
-class MultiplyOperation {
+class MultiplyOperation : AbstractOperation {
     func operate(firstNum: Int, secondNum: Int) -> Double {
         return Double(firstNum * secondNum)
     }
 }
 
-class DivideOperation {
+class DivideOperation : AbstractOperation {
     func operate(firstNum: Int, secondNum: Int) -> Double {
         return Double(firstNum / secondNum)
     }
 }
 
-class RemainderOperation {
+class RemainderOperation : AbstractOperation {
     func operate(firstNum: Int, secondNum: Int) -> Double {
         return Double(firstNum % secondNum)
     }
@@ -73,17 +54,21 @@ let multiplyOperation = MultiplyOperation()
 let divideOperation = DivideOperation()
 let remainderOperation = RemainderOperation()
 
-let calculator = Calculator(addOperation: addOperation,
-                            subtractOperation: subtractOperation,
-                            multiplyOperation: multiplyOperation,
-                            divideOperation: divideOperation,
-                            remainderOperation: remainderOperation)
+let calculator = Calculator(operation: addOperation)
 
-let addResult = calculator.calculate(oper: "+", firstNum: 10, secondNum: 5)
-let subtractResult = calculator.calculate(oper: "-", firstNum: 10, secondNum: 5)
-let multiplyResult = calculator.calculate(oper: "*", firstNum: 10, secondNum: 5)
-let divideResult = calculator.calculate(oper: "/", firstNum: 10, secondNum: 5)
-let remainderResult = calculator.calculate(oper: "%", firstNum: 10, secondNum: 5)
+let addResult = calculator.calculate(firstNum: 10, secondNum: 5)
+
+calculator.changeOperation(operation: subtractOperation)
+let subtractResult = calculator.calculate(firstNum: 10, secondNum: 5)
+
+calculator.changeOperation(operation: multiplyOperation)
+let multiplyResult = calculator.calculate(firstNum: 10, secondNum: 5)
+
+calculator.changeOperation(operation: divideOperation)
+let divideResult = calculator.calculate(firstNum: 10, secondNum: 5)
+
+calculator.changeOperation(operation: remainderOperation)
+let remainderResult = calculator.calculate(firstNum: 10, secondNum: 5)
 
 print("addResult : \(addResult)")
 print("subtractResult : \(subtractResult)")
