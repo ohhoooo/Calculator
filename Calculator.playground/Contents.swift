59d04cac5e2e8d148810f3e9031e0438545171c2
KJH <gh7052@gmail.com>
2024-03-07 13:38:58 +0900

    Swift 문법 기초 과제 - 계산기 Lv2

--- Calculator.playground/Contents.swift
+++ Calculator.playground/Contents.swift
@@ -9,6 +9,8 @@ class Calculator {
             return Double(firstNum * secondNum)
         case "/":
             return Double(firstNum / secondNum)
+        case "%":
+            return Double(firstNum % secondNum)
         default:
             return 0
         }
@@ -20,8 +22,10 @@ let addResult = calculator.calculate(oper: "+", firstNum: 10, secondNum: 5)
 let subtractResult = calculator.calculate(oper: "-", firstNum: 10, secondNum: 5)
 let multiplyResult = calculator.calculate(oper: "*", firstNum: 10, secondNum: 5)
 let divideResult = calculator.calculate(oper: "/", firstNum: 10, secondNum: 5)
+let remainderResult = calculator.calculate(oper: "%", firstNum: 10, secondNum: 5)
 
 print("addResult : \(addResult)")
 print("subtractResult : \(subtractResult)")
 print("multiplyResult : \(multiplyResult)")
 print("divideResult : \(divideResult)")
+print("remainderResult : \(remainderResult)")
