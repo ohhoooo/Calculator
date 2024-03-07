91565415a89e0584e00da8001f57db770c32004c
KJH <gh7052@gmail.com>
2024-03-07 14:11:01 +0900

    Swift 문법 기초 과제 - 계산기 Lv3

--- Calculator.playground/Contents.swift
+++ Calculator.playground/Contents.swift
@@ -1,23 +1,84 @@
 class Calculator {
+    let addOperation: AddOperation
+    let subtractOperation: SubtractOperation
+    let multiplyOperation: MultiplyOperation
+    let divideOperation: DivideOperation
+    let remainderOperation: RemainderOperation
+    
+    init(
+        addOperation: AddOperation,
+        subtractOperation: SubtractOperation,
+        multiplyOperation: MultiplyOperation,
+        divideOperation: DivideOperation,
+        remainderOperation: RemainderOperation
+    ) {
+        self.addOperation = addOperation
+        self.subtractOperation = subtractOperation
+        self.multiplyOperation = multiplyOperation
+        self.divideOperation = divideOperation
+        self.remainderOperation = remainderOperation
+    }
+    
     func calculate(oper: String, firstNum: Int, secondNum: Int) -> Double {
         switch oper {
         case "+":
-            return Double(firstNum + secondNum)
+            return addOperation.operate(firstNum: firstNum, secondNum: secondNum)
         case "-":
-            return Double(firstNum - secondNum)
+            return subtractOperation.operate(firstNum: firstNum, secondNum: secondNum)
         case "*":
-            return Double(firstNum * secondNum)
+            return multiplyOperation.operate(firstNum: firstNum, secondNum: secondNum)
         case "/":
-            return Double(firstNum / secondNum)
+            return divideOperation.operate(firstNum: firstNum, secondNum: secondNum)
         case "%":
-            return Double(firstNum % secondNum)
+            return remainderOperation.operate(firstNum: firstNum, secondNum: secondNum)
         default:
             return 0
         }
     }
 }
 
-let calculator = Calculator()
+class AddOperation {
+    func operate(firstNum: Int, secondNum: Int) -> Double {
+        return Double(firstNum + secondNum)
+    }
+}
+
+class SubtractOperation {
+    func operate(firstNum: Int, secondNum: Int) -> Double {
+        return Double(firstNum - secondNum)
+    }
+}
+
+class MultiplyOperation {
+    func operate(firstNum: Int, secondNum: Int) -> Double {
+        return Double(firstNum * secondNum)
+    }
+}
+
+class DivideOperation {
+    func operate(firstNum: Int, secondNum: Int) -> Double {
+        return Double(firstNum / secondNum)
+    }
+}
+
+class RemainderOperation {
+    func operate(firstNum: Int, secondNum: Int) -> Double {
+        return Double(firstNum % secondNum)
+    }
+}
+
+let addOperation = AddOperation()
+let subtractOperation = SubtractOperation()
+let multiplyOperation = MultiplyOperation()
+let divideOperation = DivideOperation()
+let remainderOperation = RemainderOperation()
+
+let calculator = Calculator(addOperation: addOperation,
+                            subtractOperation: subtractOperation,
+                            multiplyOperation: multiplyOperation,
+                            divideOperation: divideOperation,
+                            remainderOperation: remainderOperation)
+
 let addResult = calculator.calculate(oper: "+", firstNum: 10, secondNum: 5)
 let subtractResult = calculator.calculate(oper: "-", firstNum: 10, secondNum: 5)
 let multiplyResult = calculator.calculate(oper: "*", firstNum: 10, secondNum: 5)
